c5ca91bd1e37352b978e2144cbf3fe6fb8d65266
Luis Miguel Leon Valenzuela <luiscleverboy@gmail.com>
2014-08-22 12:44:21 -0400

    validación de Tarea 1

--- Tarea 1/ejercicio3.py
+++ Tarea 1/ejercicio3.py
@@ -1,17 +1,20 @@
 
-
-
-var = int(input("numero:"))
-#o
-#var = 600851475143
-maximo = 2;
-while var > maximo:
-    if var % maximo == 0:
-        var = var/maximo
-        maximo = 2
-    else:
-        maximo+=1;
-print("maximo: %d" % (maximo));
+while True:
+    try:
+        var = int(input("numero:"))
+        #o
+        #var = 600851475143
+        maximo = 2;
+        while var > maximo:
+            if var % maximo == 0:
+                var = var/maximo
+                maximo = 2
+            else:
+                maximo+=1;
+        print("maximo: %d" % (maximo))
+        break 
+    except ValueError: 
+        print ("ingrese un numero")
 
 
 
